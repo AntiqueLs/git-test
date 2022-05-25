a87fb94a4134a0271cea1b0772876e19d7184c88
Antique <544001794@qq.com>
2022-05-25 14:43:37 +0800

    second commit

--- src/main/java/com/antique/GitTest.java
+++ src/main/java/com/antique/GitTest.java
@@ -3,5 +3,7 @@ package com.antique;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello, Git! Hello, Git!");
+
+        System.out.println("Hello, Git222! Hello, Git222!");
     }
 }
