567b4d17d0e0260bdac5e262c12485cd311be30d
AntiqueLs <105911885+AntiqueLs@users.noreply.github.com>
2022-05-25 15:45:51 +0800

    Update GitTest.java
    master commit 2222

--- src/main/java/com/antique/GitTest.java
+++ src/main/java/com/antique/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
         System.out.println("Hello, Git333! Hello, Git333!");
 
         System.out.println("Hello, hot-fix test");
-        System.out.println("Hello, master test");
+        System.out.println("Hello, master test 222222");
     }
 }
